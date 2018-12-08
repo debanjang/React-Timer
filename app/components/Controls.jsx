ca951ea8a7bd454a2a404678e2f78ca8631ebd95
Debanjan Ghosh <debanjang@ymail.com>
2018-12-09 00:54:02 +0530

    Wire up Controls Component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -2,23 +2,32 @@ var React = require('react');
 
 var Controls = React.createClass({
     propTypes:{
-        countdownStatus: React.PropTypes.string.isRequired
+        countdownStatus: React.PropTypes.string.isRequired,
+        onStatusChange: React.PropTypes.func.isRequired
+    },
+
+    onStatusChange: function(newStatus){
+        //returning a function from a function-> Currying pattern
+        return ()=>{
+            this.props.onStatusChange(newStatus);
+        }
     },
 
     render: function(){
         var{countdownStatus} = this.props;
+        var that = this;
         function renderAppropriateButton(){
             if(countdownStatus === 'started'){
-                return (<button className="button secondary">Pause</button>);
+                return (<button className="button secondary" onClick={that.onStatusChange('paused')}>Pause</button>);
             }else if(countdownStatus === 'paused'){
-                return (<button className="button primary">Start</button>)
+                return (<button className="button primary" onClick={that.onStatusChange('started')}>Start</button>);
             }
         }
         
         return(
             <div className="controls">
                 {renderAppropriateButton()}
-                <button className="button alert hollow">Clear</button>
+                <button className="button alert hollow" onClick={this.onStatusChange('stopped')}>Clear</button>
             </div>
         );
     }
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,40 +1,10 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
 
-    /* 
-    ** componentDidUpdate is called when either it's own state or the props passed from the parent
-    ** change. This is not called during initial render. This is called just after
-    ** componentDidMount
-    */
-    componentDidUpdate: function(prevProps, prevState){
-        /* 
-        ** This check is required. First is that we only want to start the timer
-        ** when the countdownStatus changes and not when anything else does.
-        ** Second and the most important is that this method calls shouldComponentUpdate internally
-        ** which defaults to true.  As such, we get into an infinite loop as everytime we change
-        ** the state in startTimer, shouldComponentUpdate is called which triggers a background 
-        ** re-render that seriously hampers the performance
-        */
-        if(this.state.countdownStatus != prevState.countdownStatus){
-            switch(this.state.countdownStatus){
-                case 'started':
-                    this.startTimer();
-                    break;
-            }
-        }
-    },
-
-    startTimer: function(){
-        this.timer = setInterval(()=>{
-            var newCount = this.state.count-1;
-            this.setState({
-                count: newCount >= 0 ? newCount : 0
-            });
-        },1000);
-    },
     
     getInitialState: function(){
         return {
@@ -50,12 +20,68 @@ var Countdown = React.createClass({
         });
     },
 
+    handleStatusChange: function(status){
+        this.setState({
+            countdownStatus: status
+        });
+    },
+
+    /* 
+    ** componentDidUpdate is called when either it's own state or the props passed from the parent
+    ** change. This is not called during initial render. This is called just after
+    ** componentDidMount
+    */
+   componentDidUpdate: function(prevProps, prevState){
+        /* 
+        ** This check is required. First is that we only want to start the timer
+        ** when the countdownStatus changes and not when anything else does.
+        ** Second and the most important is that this method calls shouldComponentUpdate internally
+        ** which defaults to true.  As such, we get into an infinite loop as everytime we change
+        ** the state in startTimer, shouldComponentUpdate is called which triggers a background 
+        ** re-render that seriously hampers the performance
+        */
+        if(this.state.countdownStatus != prevState.countdownStatus){
+            switch(this.state.countdownStatus){
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({count:0});
+                case 'paused':
+                    clearInterval(this.timer);
+                    this.timer = undefined;
+            }
+        }
+    },
+
+    startTimer: function(){
+        this.timer = setInterval(()=>{
+            var newCount = this.state.count-1;
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            });
+            if(newCount===0){
+                this.setState({
+                    countdownStatus: 'stopped'
+                });
+            }
+        },1000);
+    },
+
     render: function(){
-        var {count} = this.state;
+        var {count, countdownStatus} = this.state;
+
+        var renderControlArea = ()=>{
+            if(countdownStatus!== 'stopped'){
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+            }else{
+                return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+            }
+        }
         return(
             <div>
                 <Clock totalSeconds={count}/>
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+                {renderControlArea()}
             </div>
         );
     }
